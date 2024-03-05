6c67b3e201fb968e6940b3cb995074335335ee5e
umair-aligtk <156018837+umair-aligtk@users.noreply.github.com>
2024-03-05 09:07:54 +0500

    Initial commit

---
+++ app.js
@@ -0,0 +1,352 @@
+
+// Chapter :4
+// Q no:1
+// var a = ""
+// document.write(a)
+
+// var b = ""
+// document.write(b)
+
+// var c = "umairali0g0@gmail.com"
+// document.write(c)
+
+
+
+// Q no:2
+// Legal variables:
+//  var legalone = ""
+//  document.write(legalone)
+
+//  var legaltwo = ""
+//  document.write(legaltwo)
+
+//  var legalthree = "legal vriables"
+//  document.write(legalthree)
+
+
+
+//  var legalfour = ""
+//  document.write(legalfour)
+
+
+//  var legalfive = ""
+//  document.write(legalfive)
+
+
+
+
+//  Q no:2
+// illegal variables:
+//  var 1variables = ""
+//  document.write(1variables)
+
+//  var alert = ""
+//  document.write(alert)
+
+//  var $myvariables = "legal vriables"
+//  document.write($myvariables)
+
+
+
+//  var var = ""
+//  document.write(var)
+
+
+//  var my-variable = ""
+//  document.write(my-variable)
+
+// Qno:3
+// (a)
+// var heading ="Rules for naming JS variables"
+// document.write("<h1>"+heading+"</h1>")
+// (b)
+// var names ="Variable names can only contain numbers $ and, _ For example:$ my-first variable" 
+// document.write("<p>"+names+"</p>")
+// (c)
+// var begins ="Variables must begin with a $name,-name,Or name"
+// document.write("<p>"+begins+"</p>")
+// (d)
+// var sensitive ="Variable names are case sensitive"
+// document.write("<p>"+sensitive+"</p>")
+// (e)
+// var js ="Variable names should not be JS Keywords"
+// document.write("<p>"+js+"</p>")
+
+
+
+// chapter 5
+// Qno:1
+// var num1 = 5
+// var num2 =3
+// var result = num1+num2
+// alert(result)
+
+// var sum ="Sum of 3 and 5 is 8"
+// document.write(sum)
+
+
+// var num1 = 8
+//  var num2 =16
+//  var result = num1-num2
+//  alert(result)
+
+// var num1 = 2
+//   var num2 =4
+//   var result = num1*num2
+//   alert(result)
+
+// var num1 = 16
+//   var num2 =2
+//   var result = num1/num2
+//   alert(result)
+
+
+// var g ="Value of variable after decleraion is Un diffined <br>"
+// document.write(g)
+
+//  var num = "Initial value:5 <br> "
+//  document.write(num)
+
+//  var num1 = 5
+//  alert(num1)
+
+// num1++
+// alert(num1)
+
+// var after ="Value after increment is 6 <br>"
+// document.write(after)
+
+// var num3=7
+
+// var result =num1 +num3
+// alert(result)
+
+// var add ="Value after addition is 13 <br>"
+// document.write(add)
+
+// result--
+// alert(result)
+
+
+// var dec ="Value after decrement is 12 <br>"
+// document.write(dec)
+
+// var num4 = 3
+
+// var reminder = result % num4
+// alert(reminder)
+
+// var zero = "The reminder is zero"
+// document.write(zero)
+
+
+// var ticket = 600
+// var total=5
+// var result = ticket* total
+// document.write(result)
+
+// var text ="Total cost to buy 5 tickets to a movie is 3000pkr"
+// document.write(text)
+
+// var table ="Table of 4<br>"
+// document.write(table)
+
+// var one = "4 x 2 = 8 <br>"
+// document.write(one)
+
+// var one = "4 x 3 = 12 <br>"
+// document.write(one)
+
+// var one = "4 x 4 = 16 <br>"
+// document.write(one)
+
+// var one = "4 x 5 = 20 <br>"
+// document.write(one)
+
+// var one = "4 x 6 = 24 <br>"
+// document.write(one)
+
+// var one = "4 x 7 = 28 <br>"
+// document.write(one)
+
+// var one = "4 x 8 = 32 <br>"
+// document.write(one)
+
+// var one = "4 x 9 = 36 <br>"
+// document.write(one)
+
+// var one = "4 x 10 = 40 <br>"
+// document.write(one)
+
+//   var celcius = 37
+//   var formulla =(celcius * 9/5)+32
+//  var faranite = celcius*9/5 +32
+//   document.write(faranite )
+
+
+// var calcius2 =(98.6 -32) * 5/9
+// document.write(calcius2)
+
+// var text = "37 Celcius is equal to is 98.6 Faranite"
+//  document.write(text)
+
+// var cart ="Shopping Cart"
+// document.write("<h1>"+cart+"</h1>")
+
+
+//    var item1 = "price of one item is 650 <br>"
+//   document.write(item1)
+//    var item3 = 650
+//    var quantity ="Quantity of item is 3 = "
+
+
+//   var item2 = quantity+ item3*3 
+//   document.write(item2)
+
+
+
+ 
+//     var item4 = "<br>price of one  item2 is 100 <br>"
+//    document.write(item4)
+// var item5 = 100
+
+// var quantity ="Quantity of item2 is 7 =  "
+// var item6 = quantity+item5*7
+// document.write(item6)
+ 
+// var shipping =" <br>shipping charges 100 <br>"
+// document.write(shipping)
+//  var ship =100
+
+ 
+ 
+//  var result =  item3*3 +item5*7 +ship
+//  document.write(result)
+//  var total =" <br>Total cost of your order is ="
+
+//  var resultfinal = total + result
+// document.write(resultfinal)
+
+
+// Qno:8
+
+// var total = 980;
+// var obtained = 804;
+
+
+// // var result = obtained/total *100
+// // document.write(result)
+
+// var text ="Marks sheet"
+// document.write("<h1>"+text+"</h1>")
+
+//  var totalmarks ="Total marks:980 <br>obtained marks:804 <br> Percentage: 82.0408163265306"
+//  document.write(totalmarks)
+
+//  var us = 10
+//  var riyals = 25
+//  var result =  us*280 + riyals*70
+// document.write(result)
+
+// var text1 = " Currency in PKR"
+// document.write("<h1>"+text1+ "</h1>")
+
+// var text = "<br> Total currency in PKR ="
+
+//  Qno:10
+// var final = text + result
+// document.write(final)
+// var add = 50;
+// var result =50+5*10/2
+// document.write(result)
+
+// Qno:11
+// var text1 = " Age calculator"
+// document.write("<h1>"+text1+ "</h1>")
+// var current = 2024
+// var birth=2007
+// var result = current-birth
+// document.write(result)
+
+// var final = "Current year: 2024 <br>Birth year:2007 <br> your age is: 17"
+// document.write(final)
+
+
+// var radius=  "Radius of circle is 20 <br>  "
+// var circle=20;
+// document.write(radius)
+// var result = " The circumferance is: " +circle*6.28
+// document.write(result)
+
+// var area = 20
+// var final = " <br>The area is:1256.8" +area* 3.
+// document.write(final)
+
+
+// var snack = " Favourite snacks:lays <br>"
+// document.write(snack)
+// var age = "Current age:17 <br>"
+// document.write(age)
+// var maxage = "Estimated maximum age:67 <br>"
+// document.write(maxage)
+// var amount = " Amount of snack per day:3 <br>"
+// document.write(amount)
+// var final = 3*50
+// document.write(final)
+//  var result1 = "You need to 150 lays to ast you untill the ripe old age of 67"
+//  document.write(result1)
+
+// Chapter :9
+// Qno:1
+// var result ="result: <br> The value of a is:10 <br> ...................................";
+// document.write(result)
+// var  a =  10 ; 
+// a++
+// document.write(a)
+// var text =  " <br>The value of ++a is 11 "
+// document.write(text)
+// var a1 = "<br>Now the value a is 11 "
+// document.write(a1)
+
+// var text =  " <br>The value of ++a is 11 "
+// document.write(text)
+// var a1 = "<br>Now the value a is 12 "
+// document.write(a1)
+
+// //  var  b =  12 ; 
+// //  b--
+// //  document.write(b)
+
+//  var text =  " <br>The value of --a is: 11 "
+// document.write(text)
+// var a1 = "<br>Now the value a is 11 "
+// document.write(a1)
+// // var  b =  12 ; 
+// // b--
+// // document.write(b)
+// var text =  " <br>The value of a-- is: 11 "
+// document.write(text)
+// var a1 = "<br>Now the value a is 10 "
+// document.write(a1)
+// var plus2 = "--a - --b + ++b + b--; <br>"
+// document.write(plus2)
+// var plus1 = "1 - 0 + 2 + 2 <br>"
+// document.write(plus1)
+// var plus = "--a=1    <br> --b=0   <br> ++b=2   <br> b--=1"
+// document.write(plus)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
